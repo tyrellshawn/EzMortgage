478d806e5ff05a9e0e7db12d6b644a1125977ded
tyrellshawn <ooluk098@uottawa.ca>
2020-01-10 23:21:39 -0500

    Added Trailing Icons for Principal and Years, todo create Dialog that provides users a definition for the Values.

--- app/src/main/java/com/olaolukiyesi/myapplication/MainActivity.java
+++ app/src/main/java/com/olaolukiyesi/myapplication/MainActivity.java
@@ -3,12 +3,27 @@ package com.olaolukiyesi.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+ TextInputLayout mPrincipalLayout;
+ TextInputLayout mNumberofYearsLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPrincipalLayout = findViewById(R.id.txtPrincipal_layout);
+        mNumberofYearsLayout = findViewById(R.id.txtYears_layout);
+
+        mPrincipalLayout.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //new MaterialAlertDialogBuilder(getApplicationContext(),R.style.ThemeOverlay_MaterialComponents_MaterialAlertDialog).setTitle("Principal").setMessage("The amount due minus the down Payment.").setPositiveButton("Ok",null).show();
+            }
+        });
     }
 }
