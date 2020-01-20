2edc271bb55da9f2cea4fc0109393c0a74c5fa1e
tyrellshawn <ooluk098@uottawa.ca>
2020-01-20 16:58:58 -0500

    Fixed Main View, Added Course A to the db.json

--- app/src/main/java/com/olaolukiyesi/myapplication/MainActivity.java
+++ app/src/main/java/com/olaolukiyesi/myapplication/MainActivity.java
@@ -9,21 +9,11 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
- TextInputLayout mPrincipalLayout;
- TextInputLayout mNumberofYearsLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mPrincipalLayout = findViewById(R.id.txtPrincipal_layout);
-        mNumberofYearsLayout = findViewById(R.id.txtYears_layout);
 
-        mPrincipalLayout.setEndIconOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //new MaterialAlertDialogBuilder(getApplicationContext(),R.style.ThemeOverlay_MaterialComponents_MaterialAlertDialog).setTitle("Principal").setMessage("The amount due minus the down Payment.").setPositiveButton("Ok",null).show();
-            }
-        });
     }
 }
